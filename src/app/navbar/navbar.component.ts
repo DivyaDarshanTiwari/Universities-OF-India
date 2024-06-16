41e374f061a5afba8ef7d558accff596dff1f0fd
DivyaDarshanTiwari <davidrocco400@outlook.com>
2024-06-16 17:23:01 +0530

    removed images

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule,MatIconModule,MatDividerModule,MatButtonModule],
   template: `
-    <p>
-      navbar works!
-    </p>
+      <mat-toolbar color="primary">
+    <span>Bit-Coin Desu</span>
+    <div class="spacer"></div>
+      <a mat-button>Home</a>
+      <a mat-button>About</a>
+      <a mat-button>Contact</a>
+    </mat-toolbar>
   `,
   styleUrl: './navbar.component.css'
 })
