3a176b838009d202855d048b1c5b46e1d52ba838
DivyaDarshanTiwari <davidrocco400@outlook.com>
2024-06-16 17:04:01 +0530

    madde cards+css

--- src/app/unviversity-section/unviversity-section.component.ts
+++ src/app/unviversity-section/unviversity-section.component.ts
@@ -5,11 +5,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { OnInit } from '@angular/core';
 import { NgFor } from '@angular/common';
 import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-unviversity-section',
   standalone: true,
-  imports: [HttpClientModule,NgFor,MatGridListModule],
+  imports: [HttpClientModule,NgFor,MatGridListModule,MatCardModule],
   providers: [UserDataService],
   templateUrl: './unviversity-section.component.html',
   styleUrl: './unviversity-section.component.css'
@@ -17,18 +18,22 @@ import {MatGridListModule} from '@angular/material/grid-list';
 export class UnviversitySectionComponent {
 
   userdata: any;
+
+  state: string;
   
   constructor(private data:UserDataService)
   {
-
+    this.state='';
   }
 
   getData(){
     this.data.users().subscribe((data)=>{
       this.userdata = data;
       console.log(data);
+      console.log(data)
     })
   }
+
 
   ngOnInit()
   {
