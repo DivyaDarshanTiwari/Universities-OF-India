61decaee8692d5dbd3f03d912734f4bca9461b2f
DivyaDarshanTiwari <davidrocco400@outlook.com>
2024-06-16 15:26:01 +0530

    Added angular material

--- src/app/unviversity-section/unviversity-section.component.ts
+++ src/app/unviversity-section/unviversity-section.component.ts
@@ -1,16 +1,37 @@
 import { Component } from '@angular/core';
+import { UserDataService } from '../services/user-data.service';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { OnInit } from '@angular/core';
+import { NgFor } from '@angular/common';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-unviversity-section',
   standalone: true,
-  imports: [],
-  template: `
-    <p>
-      unviversity-section works!
-    </p>
-  `,
+  imports: [HttpClientModule,NgFor,MatGridListModule],
+  providers: [UserDataService],
+  templateUrl: './unviversity-section.component.html',
   styleUrl: './unviversity-section.component.css'
 })
 export class UnviversitySectionComponent {
 
+  userdata: any;
+  
+  constructor(private data:UserDataService)
+  {
+
+  }
+
+  getData(){
+    this.data.users().subscribe((data)=>{
+      this.userdata = data;
+      console.log(data);
+    })
+  }
+
+  ngOnInit()
+  {
+    this.getData();
+  }
 }
